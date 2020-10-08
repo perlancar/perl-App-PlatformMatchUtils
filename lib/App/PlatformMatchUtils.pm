975f4d4ef92ce9816890a885f9e0c4a55d31d4fb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-08 15:24:10 +0700

    Initial commit

---
+++ lib/App/PlatformMatchUtils.pm
@@ -0,0 +1,20 @@
+package App::PlatformMatchUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Utilities related to Devel::Platform::Match
+
+=head1 DESCRIPTION
+
+This distribution provides the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Devel::Platform::Match>
